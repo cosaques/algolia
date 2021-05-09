31a9b72a434c58f162380a96bf5d6f098c3d698f
Artem MINIAILO <aminiailo@vente-privee.com>
2021-05-09 07:53:48 +0200

    optimized version of string cache

--- indexer/stringCache.go
+++ indexer/stringCache.go
@@ -2,9 +2,25 @@ package indexer
 
 import "sync"
 
-var stringCache sync.Map
+var stringCache map[string]*string = make(map[string]*string)
+var lock = sync.RWMutex{}
 
 func LoadOrStoreStringPtr(s string) *string {
-	p, _ := stringCache.LoadOrStore(s, &s)
-	return p.(*string)
+	lock.RLock()
+	if p, exist := stringCache[s]; !exist {
+		// The string wasn't found, so we'll create it.
+		lock.RUnlock()
+		lock.Lock()
+		defer lock.Unlock()
+		if p, exist := stringCache[s]; !exist {
+			// Insert the new string.
+			stringCache[s] = &s
+			return &s
+		} else {
+			return p
+		}
+	} else {
+		lock.RUnlock()
+		return p
+	}
 }
