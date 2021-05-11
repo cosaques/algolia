fd32bb7e143bafbe2387bfd8abbd4c2a720fb725
Artem MINIAILO <aminiailo@vente-privee.com>
2021-05-11 11:58:48 +0200

    added cmd test

--- main.go
+++ main.go
@@ -1,12 +1,85 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/cosaques/algolia/indexer"
+)
 
 func main() {
-	m := map[string]int{"artem": 1}
-	if m, exist := m["artem2"]; !exist {
-		fmt.Println("not exists", m)
-	} else {
-		fmt.Println(m)
+	ch := make(chan struct{})
+	go func() {
+		var i int
+		for {
+			if _, ok := <-ch; !ok {
+				return
+			}
+
+			i++
+			fmt.Printf("\rIndexed %d", i)
+		}
+	}()
+
+	file, _ := os.Open("hn_logs.tsv")
+	defer file.Close()
+	traceReader := indexer.NewTraceReader(file)
+	aggregator := indexer.NewAggregator()
+	var wg sync.WaitGroup
+	for trace, err := traceReader.Read(); !errors.Is(err, io.EOF); trace, err = traceReader.Read() {
+		if err != nil {
+			panic(err)
+		}
+		wg.Add(1)
+		go func(trace indexer.Trace) {
+			defer wg.Done()
+			aggregator.Add(trace)
+			ch <- struct{}{}
+		}(trace)
 	}
+	wg.Wait()
+	close(ch)
+
+	fmt.Println("\nCompleted!")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Query type (L|T): ")
+		scanner.Scan()
+		query := scanner.Text()
+		fmt.Printf("%q\n", query)
+		switch query {
+		case "L":
+			fmt.Print("Date range: ")
+			scanner.Scan()
+			date := scanner.Text()
+			tr, _ := indexer.ParseTimeRange(date)
+			if idx := aggregator.GetIndex(tr); idx != nil {
+				fmt.Println(idx.Len())
+			} else {
+				fmt.Println(0)
+			}
+
+		case "T":
+			fmt.Print("Size: ")
+			scanner.Scan()
+			size, _ := strconv.Atoi(scanner.Text())
+			fmt.Println(size)
+			fmt.Print("Date range: ")
+			scanner.Scan()
+			date := scanner.Text()
+			tr, _ := indexer.ParseTimeRange(date)
+			if idx := aggregator.GetIndex(tr); idx != nil {
+				fmt.Println(idx.Top(size))
+			} else {
+				fmt.Println("[]")
+			}
+		}
+	}
+
 }
